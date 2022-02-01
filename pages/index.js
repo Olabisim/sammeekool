c3f46053aba4e556d1e9e8486f24f523b7a8c60d
holabisii <ajoseholabisi@gmail.com>
2022-02-01 15:35:45 -0500

    completed setup with header component

---
+++ pages/index.js
@@ -0,0 +1,14 @@
+import {Header, Footer} from '../components/Layout'
+
+const Index = () => {
+        return (
+
+                <div>
+                        <Header />
+                        this is the home page
+                        <Footer />
+                </div>
+        )
+}
+
+export default Index;
